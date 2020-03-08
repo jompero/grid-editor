429a57aeb8375597497c129c1fd905f04c344074
Dani Jompero <dani.jompero@ubisoft.com>
2020-03-08 21:32:32 +0200

    Project pipeline setup

---
+++ backend/build/precache-manifest.868e7b77f70246deb9a521b4e0277037.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49a00708bb9ecefa9aa06f62ad11a1ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4adbf5be2f73649f5dfc",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b3ae35249bca5ac95a2d",
+    "url": "/static/js/2.017da106.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.017da106.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4adbf5be2f73649f5dfc",
+    "url": "/static/js/main.9fa1dc9b.chunk.js"
+  },
+  {
+    "revision": "5a33a1cc73a89e9a2e92",
+    "url": "/static/js/runtime-main.8e414355.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
