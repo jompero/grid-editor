fb87c64751ce62f760e0c681204898f520863dc6
Dani Jompero <dani.jompero@ubisoft.com>
2020-03-10 21:36:31 +0200

    Testing Material UI

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,16 @@
 import * as React from 'react';
+import { AppBar, Toolbar, Typography } from '@material-ui/core';
 
 function App() {
   return (
     <div className="App">
-      Grid Editor
+      <AppBar position='static'>
+        <Toolbar>
+          <Typography variant="h6">
+            Grid Editor
+          </Typography>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 }
