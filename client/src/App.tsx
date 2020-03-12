e4bfcbef30aa05ffdb923a90232dc4645941a907
Dani Jompero <dani.jompero@ubisoft.com>
2020-03-12 22:10:35 +0200

    TileMap attempt

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,12 @@
 import * as React from 'react';
 import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import Tile from './components/Tile';
+import image from './9445.png';
 
 function App() {
+
+
+
   return (
     <div className="App">
       <AppBar position='static'>
@@ -11,8 +16,17 @@ function App() {
           </Typography>
         </Toolbar>
       </AppBar>
+      {tileMap}
     </div>
   );
 }
 
 export default App;
+
+function tileMap(): any {
+  let tiles = [];
+  for (let i = 0; i < 10; i++) {
+    tiles.push(<Tile image={image} posX={i} posY={i} />)
+  }
+  return tiles;
+}
---
+++ client/src/components/Tile.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+
+export interface Props {
+  image: string;
+  posX: number;
+  posY: number;
+}
+
+function Tile({ image, posX, posY }: Props) {
+
+  const style = {
+    backgroundImage: `url(${image})`,
+    transform: 'scale(2)',
+    transformOrigin: 'top left',
+    backgroundPosition: `-${posX}px -${posY}px`,
+    width: '16px',
+    height: '16px'
+  }
+
+  return (
+    <div>
+      <div style={style} />
+    </div>
+  );
+}
+
+export default Tile;
