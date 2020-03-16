6aafdff6d62407f91d6f95b22063c7e9b3f67f56
Dani Jompero <dani.jompero@ubisoft.com>
2020-03-16 22:22:36 +0200

    Palette and broken painting

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,30 @@
 import * as React from 'react';
-import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import { AppBar, Toolbar, Typography, Paper, Button, Drawer } from '@material-ui/core';
 import Grid from './components/Grid'
 import Tile from './components/Tile';
 import image from './9445.png';
 import mapping from './9445.json';
+import { useState } from 'react';
 
 function App() {
+
+  const [open, setOpen] = useState(false);
+  const [tile, setTile] = useState(0);
+  const [tileMap, setTileMap] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+
+  function palette(): any {
+    return mapping.map((coordinate, index) => <Button onClick={() => setTile(index)}><Tile image={image} posX={coordinate['x']} posY={coordinate['y']} /></Button>);
+  }
+
+  function drawTileMap(): any {
+    return tileMap.map((t, i) => {
+      return (
+        <Button onClick={() => setTileMap(tileMap.map((oldT, index) => index !== i ? oldT : tile))}>
+          <Tile image={image} posX={mapping[t]['x']} posY={mapping[t]['y']} />
+        </Button>
+      )
+    });
+  }
 
   return (
     <div className="App">
@@ -14,27 +33,28 @@ function App() {
           <Typography variant="h6">
             Grid Editor
           </Typography>
+          <Button onClick={() => setOpen(true)}>Open Palette</Button>
+          Selected tile: {tile}
         </Toolbar>
       </AppBar>
 
-      <Grid columns={5} rows={3} tileHeight={32} tileWidth={32} >
-        {tileMap()}
-      </Grid>
+      <Paper square>
+        <Grid columns={5} rows={3} tileHeight={32} tileWidth={32} >
+          {drawTileMap()}
+        </Grid>
+      </Paper>
+
+      <Drawer anchor="right" open={open} onClose={() => setOpen(false)}>
+        <Grid columns={5} rows={3} tileHeight={32} tileWidth={32} >
+            {palette()}
+          </Grid>
+      </Drawer>
+
     </div>
   );
 }
 
 export default App;
-
-function tileMap(): any {
-  let tiles = [];
-  for (let i = 0; i < 15; i++) {
-    let coordinate: Coordinate = mapping[i%4];
-    tiles.push(<Tile image={image} posX={coordinate['x']} posY={coordinate['y']} />)
-  }
-  console.log(tiles)
-  return tiles;
-}
 
 export interface Coordinate {
   x: number,
