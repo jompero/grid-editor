3205e6e8b934a8d8e50b3be4b4aa32fc3df198ce
Dani Jompero <dani.jompero@gmail.com>
2020-04-21 20:08:41 +0300

    Update tileArrayReducer.tsx

--- client/src/reducers/tileArrayReducer.tsx
+++ client/src/reducers/tileArrayReducer.tsx
@@ -21,46 +21,49 @@ const initializeState = () => {
   };
 };
 
-const tileArrayReducer = (state: TileArrayState = initializeState(), action: TileArrayAction) => {
-    switch(action.type) {
-      case 'SET_TILE': 
-        return paint(state, action.data.index, action.data.tile);
-      case 'UNDO': 
-        if (state.current === 0) return state;
-        return { ...state, current: state.current - 1 };
-      case 'REDO':
-        if (state.current === state.history.length - 1) return state;
-        return { ...state, current: state.current + 1 };
-      default:
-      return state;
-  }
-}
-
 function paint(state: TileArrayState, index: number, tile: number): TileArrayState {
   const tiles = state.history[state.current].tiles;
   const newArray = tiles.map((oldTile, i) => i !== index ? oldTile : tile);
-  const history = [ ...state.history.slice(0, state.current + 1), new TileArray(newArray) ];
+  const history = [...state.history.slice(0, state.current + 1), new TileArray(newArray)];
   const current = state.current + 1;
   return { history, current };
+}
+
+function tileArrayReducer(
+  state: TileArrayState = initializeState(),
+  action: TileArrayAction,
+): TileArrayState {
+  switch (action.type) {
+    case 'SET_TILE':
+      return paint(state, action.data.index, action.data.tile);
+    case 'UNDO':
+      if (state.current === 0) return state;
+      return { ...state, current: state.current - 1 };
+    case 'REDO':
+      if (state.current === state.history.length - 1) return state;
+      return { ...state, current: state.current + 1 };
+    default:
+      return state;
+  }
 }
 
 export function paintTile(index: number, tile: number) {
   return {
     type: 'SET_TILE',
-    data: { index, tile }
-  }
+    data: { index, tile },
+  };
 }
 
 export function undo() {
   return {
-    type: 'UNDO'
-  }
+    type: 'UNDO',
+  };
 }
 
 export function redo() {
   return {
-    type: 'REDO'
-  }
+    type: 'REDO',
+  };
 }
 
 export default tileArrayReducer;
