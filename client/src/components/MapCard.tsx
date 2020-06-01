e89a0eca3e7a356024c76a4c044b1a0d3744b4aa
Dani Jompero <dani.jompero@gmail.com>
2020-06-01 21:16:26 +0300

    Code clean up

--- client/src/components/MapCard.tsx
+++ client/src/components/MapCard.tsx
@@ -3,7 +3,7 @@ import { Card, CardActionArea, CardMedia, CardContent, Typography, CardActions, 
 import Grid from './Grid';
 import { TileMap, deleteMap } from '../services/mapsService';
 import { Link, useHistory } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { deleteMap as cutMap } from '../reducers/mapsReducer';
 import Tile from './Tile';
 import { load } from '../reducers/canvasReducer';
