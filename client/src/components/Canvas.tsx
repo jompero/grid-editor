57a204a379c835b75ea6e9617a1e3511c6f71e1f
Dani Jompero <dani.jompero@gmail.com>
2020-06-10 21:40:46 +0300

    Minor performance improvement to Canvas

--- client/src/components/Canvas.tsx
+++ client/src/components/Canvas.tsx
@@ -2,17 +2,17 @@ import * as React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { paintTile } from '../reducers/canvasReducer';
 import Grid from './Grid';
-import Tile from './Tile';
 import { RootState } from '../store';
 import tileSets from '../services/tileSets';
+import ClickableTile from './ClickableTile';
 
 function Canvas() {
   const dispatch = useDispatch();
-  const canvas = useSelector((state: RootState) => state.canvas);
+  const canvas = useSelector((state: RootState) => state.canvas.history[state.canvas.current]);
+  const { width, height } = useSelector((state: RootState) => state.canvas.tileMap);
   const brush = useSelector((state: RootState) => state.tools.brush);
   const tileSetName = useSelector((state: RootState) => state.tileSet);
   const tileSet = tileSets[tileSetName];
-  const mapping = tileSet.mapping;
 
   const [click, setClick] = React.useState(false);
 
@@ -26,36 +26,16 @@ function Canvas() {
     setClick(false);
   }
 
-  function getColor(index: number, columns: number) {
-    const tileValue = (index % columns) + Math.floor(index / columns);
-    //console.log('index', index, 'tileValue', tileValue);
-    return {
-      color: tileValue % 2 === 0 ? 'GREY' : 'WHITE',
-      width: tileSet.tileWidth,
-      height: tileSet.tileHeight
-    }
-  }
-
   function parseTileArray() {
-    return canvas.history[canvas.current].map((tile: number, index: number) => (
-            <div
-              key={index}
-              onMouseDown={() => paint(index)}
-              onMouseUp={() => handleMouseRelease()}
-              onMouseEnter={(event) => click && paint(index)}
-            >
-              {tile >= 0 && <Tile {...mapping[tile]} />}
-              {tile === -1 && <Tile {...getColor(index, canvas.tileMap.width)}/>}
-            </div>
+    return canvas.map((tile: number, index: number) => (
+      <ClickableTile key={index} index={index} tile={tile} onMouseClick={() => paint(index)} mouseDown={click} onMouseRelease={() => handleMouseRelease()} />
     ));
   }
 
-  if (canvas.tileMap.width === 0) return null;
-
   return (
         <Grid
-          columns={canvas.tileMap.width}
-          rows={canvas.tileMap.height}
+          columns={width}
+          rows={height}
           tileHeight={tileSet.tileHeight}
           tileWidth={tileSet.tileWidth}
           scale={32/tileSet.tileWidth} >
---
+++ client/src/components/ClickableTile.tsx
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import Tile from './Tile';
+import tileSets from '../services/tileSets';
+import { useSelector } from 'react-redux';
+import { RootState } from '../store';
+
+export interface Props {
+  index: number;
+  tile: number;
+  onMouseClick: Function;
+  mouseDown: boolean;
+  onMouseRelease: Function;
+}
+
+function ClickableTile({ index, tile, mouseDown, onMouseClick, onMouseRelease }: Props) {
+  const tileSetName = useSelector((state: RootState) => state.tileSet);
+  const columns = useSelector((state: RootState) => state.canvas.tileMap.width);
+  const tileSet = tileSets[tileSetName];
+  const mapping = tileSet.mapping;
+
+  const color = () => {
+    const tileValue = (index % columns) + Math.floor(index / columns);
+    //console.log('index', index, 'tileValue', tileValue);
+    return {
+      color: tileValue % 2 === 0 ? 'GREY' : 'WHITE',
+      width: tileSet.tileWidth,
+      height: tileSet.tileHeight
+    }
+  }
+
+  return (
+    <div
+      key={index}
+      onMouseDown={() => onMouseClick()}
+      onMouseUp={() => onMouseRelease()}
+      onMouseEnter={(event) => mouseDown && onMouseClick()}
+    >
+      {tile >= 0 && <Tile {...mapping[tile]} />}
+      {tile === -1 && <Tile {...color()} />}
+    </div>
+  )
+}
+
+export default ClickableTile;
