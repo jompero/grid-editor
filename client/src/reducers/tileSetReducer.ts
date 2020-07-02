b1858fd2f6a71891c4d6bf8af3f6ac6984040ad2
Dani Jompero <dani.jompero@gmail.com>
2020-07-02 19:38:31 +0300

    Client clean up

---
+++ client/src/reducers/tileSetReducer.ts
@@ -0,0 +1,33 @@
+import tileSets from '../services/tileSets';
+import { TileMap } from '../services/mapsService';
+import Debug from '../utils/Debug';
+
+export interface TileSetAction {
+  type: string,
+  data: {
+    tileSet?: string,
+    tileMap?: TileMap
+  }
+}
+
+function tileSetReducer(state: string = 'Harbour', action: TileSetAction) {
+  switch (action.type) {
+    case 'SET_TILESET':
+      return action.data.tileSet || state;
+    case 'LOAD_MAP':
+    case 'UPDATE_MAP':
+      Debug('TileSet', action.data);
+      return action.data?.tileMap ? action.data.tileMap.tileSet : state;
+    default:
+      return state;
+  }
+}
+
+export function setTileSet(tileSet: string) {
+  return {
+    type: 'SET_TILESET',
+    data: { tileSet: tileSets[tileSet] },
+  };
+}
+
+export default tileSetReducer;
