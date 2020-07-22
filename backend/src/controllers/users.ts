3ece642d41f69ef7461ec40042fdf481a8273408
Dani Jompero <dani.jompero@ubisoft.com>
2020-07-22 21:23:53 +0300

    adds serverside login

--- backend/src/controllers/users.ts
+++ backend/src/controllers/users.ts
@@ -1,11 +1,16 @@
 import express from 'express';
 import users from '../models/user';
+import { getUser } from '../utils/google-auth';
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
   users.find({})
     .then((response) => res.send(response));
+});
+
+router.post('/', getUser, (req, res) => {
+  res.send(req.user);
 });
 
 export default router;
