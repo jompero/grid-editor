57193e65f15fdc3a47ac2365d220ccdfcf62d43b
Dani Jompero <dani.jompero@gmail.com>
2020-08-02 20:22:45 +0300

    Fixes final unit test issue

--- client/src/reducers/userReducer.test.ts
+++ client/src/reducers/userReducer.test.ts
@@ -1,17 +1,19 @@
 import '@testing-library/jest-dom/extend-expect';
 import configureStore from 'redux-mock-store';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import userReducer, { login, logout } from './userReducer';
 import users from '../../testProps/users.json'; // eslint-disable-line import/extensions
 import thunk from 'redux-thunk';
 import GridEditorThunk from '../utils/GridEditorThunk';
 import axiosMock from 'jest-mock-axios';
+import { NoUser, User } from '../services/usersService';
 
-const mockStore = configureStore<{}, GridEditorThunk>([thunk]);
+const middlewares = [ thunk ];
+const mockStore = configureStore<User, GridEditorThunk>(middlewares);
 
 describe('on action', () => {
   test('login, user is logged in', async () => {
-    const store = mockStore({});
+    const store = mockStore(() => NoUser);
     await store.dispatch(login(users[0]));
     axiosMock.mockResponse({ data: users[0] });
 
@@ -23,7 +25,7 @@ describe('on action', () => {
   });
 
   test('logout, user is logged out', () => {
-    const store = mockStore({});
+    const store = mockStore(() => NoUser);
     store.dispatch(logout());
 
     const actions = store.getActions();
@@ -34,17 +36,14 @@ describe('on action', () => {
 });
 
 describe('reducer', () => {
-  const store = createStore(userReducer);
-
-  const loggedOut = {
-    name: '',
-    profile: '',
-    token: '',
-  };
+  const store = createStore(
+    userReducer,
+    applyMiddleware(thunk),
+);
 
   test('is initialized', () => {
     const state = store.getState();
-    expect(state).toStrictEqual(loggedOut);
+    expect(state).toStrictEqual(NoUser);
   });
 
   test('can log in', async () => {
@@ -58,6 +57,6 @@ describe('reducer', () => {
   test('can log out', () => {
     store.dispatch(logout());
     const state = store.getState();
-    expect(state).toStrictEqual(loggedOut);
+    expect(state).toStrictEqual(NoUser);
   });
 });
